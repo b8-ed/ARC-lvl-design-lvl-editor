735da054ec53b16df0e0d26f95c5464a32b7a256
LB <luisbazan417@gmail.com>
2017-11-26 12:11:41 -0600

    Avances

---
+++ Assets/Lvl 3. SHMUP/Scripts/SCR_BulletBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_BulletBehaviour : MonoBehaviour {
+
+    public float yForce = 0.0f;
+    private Rigidbody2D bulletRgbd2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bulletRgbd2d = GetComponent<Rigidbody2D>();
+        bulletRgbd2d.AddForce(new Vector2(0, yForce));
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("TopBound"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Lvl 3. SHMUP/Scripts/SCR_PlayerController.cs
+++ Assets/Lvl 3. SHMUP/Scripts/SCR_PlayerController.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SCR_PlayerController : MonoBehaviour {
-
-    public GameObject goSplit;
-    public GameObject goTogether;
-
+public class SCR_PlayerController : MonoBehaviour
+{
+    [Header("Player Stats")]
     public int healthPoints;
     public int velocity;
     public float coolDown;
 
-    private bool canShoot;
+    [Header("Bullet")]
+    public GameObject goBullet;
+    public GameObject goBulletSpawn;   
+
+    private bool canShoot = true;
 
     private Rigidbody2D rb2DPlayer;
 
@@ -22,17 +24,35 @@ public class SCR_PlayerController : MonoBehaviour {
 	
 	void Update ()
     {
+        Shoot();
+        Movement();
+	}
+
+    public void Movement()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(x, y);
         transform.Translate(movement * Time.deltaTime * velocity);
-	}
+    }
 
     public void Shoot()
     {
         if(canShoot)
         {
-
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(goBullet, goBulletSpawn.transform.position, Quaternion.identity);
+                Debug.Log("I am shooting");
+                StartCoroutine(TurnOffCooldown());
+            }
         }
+    }
+
+    IEnumerator TurnOffCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(coolDown);
+        canShoot = true;
     }
 }
