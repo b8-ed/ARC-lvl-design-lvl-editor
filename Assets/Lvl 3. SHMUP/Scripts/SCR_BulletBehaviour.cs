5aea9377edb60970e7d6e747a2833633a54d9507
LB <luisbazan417@gmail.com>
2017-12-05 12:58:47 -0600

    First enemy, bullet type UI, damage to enemy addeed

--- Assets/Lvl 3. SHMUP/Scripts/SCR_BulletBehaviour.cs
+++ Assets/Lvl 3. SHMUP/Scripts/SCR_BulletBehaviour.cs
@@ -9,9 +9,8 @@ public class SCR_BulletBehaviour : MonoBehaviour {
     public float coolDown = 0.0f;
 
     private Rigidbody2D bulletRgbd2d;
-
-	// Use this for initialization
-	void Start ()
+    
+    void Start ()
     {
         bulletRgbd2d = GetComponent<Rigidbody2D>();
         bulletRgbd2d.AddForce(new Vector2(0, yForce));
@@ -21,6 +20,12 @@ public class SCR_BulletBehaviour : MonoBehaviour {
     {
         if(collision.collider.CompareTag("TopBound"))
         {
+            Destroy(gameObject);
+        }
+
+        if(collision.collider.CompareTag("Enemy"))
+        {
+            collision.collider.GetComponent<SCR_EnemyBehaviour>().TakeDamage((int)damage);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Lvl 3. SHMUP/Scripts/SCR_EnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿//Code by Luis Bazan
+//Github user: luisquid11
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_EnemyBehaviour : MonoBehaviour {
+    
+    [Header("Enemy Stats")]
+    public int healthPoints = 100;
+    public int armor;
+
+    private bool isDead = false;
+    private Animator animEnemy;
+    private int isDeadHash;
+    
+    void Start () 
+	{
+        animEnemy = GetComponent<Animator>();
+        isDeadHash = Animator.StringToHash("IsDead");
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+    public void Dead()
+    {
+        Debug.Log("I am dead");
+        animEnemy.SetTrigger(isDeadHash);
+    }
+
+    public void TakeDamage(int _damagePoints)
+    {
+        healthPoints -= _damagePoints;
+        if(healthPoints <= 0)
+        {
+            isDead = true;
+            healthPoints = 0;
+            Dead();
+        }
+    }
+}
